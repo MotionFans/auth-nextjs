221ac67741451d82eca35a961f92c714c40db20b
Josh <oracularhades@motionfans.com>
2023-12-19 02:36:22 +1300

    bug fixes

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import '../styles/global.css';
 import { redirect } from 'next/navigation';
 
 export default function Main() {
-  redirect("/home");
+  redirect("/login");
   
   return (
     <div style={{ width: "100%", height: "100vh", overflow: "hidden", alignItems: "center", justifyContent: "center" }}>
